b528504c0f2480ef167db7b2876adb9dc8c349a5
Resul <resul.ramadani@student.vinci.be>
2026-05-07 16:38:20 +0200

    Clean unused restaurant mock data

--- src/data/mockOffers.ts
+++
@@ -1,5 +0,0 @@
-import { restaurants } from "./restaurants";
-
-export const allOffers = restaurants
-  .filter((r) => r.hasOffer && r.offer)
-  .map((r) => ({ ...r.offer!, restaurantId: r.id, restaurantName: r.name, image: r.image }));
